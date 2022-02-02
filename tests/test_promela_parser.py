5c32ec8122f70c342bb15ab9a10d1b1595d867c2
Haukur Ingi Heiðarsson <haukur@heidarsson.is>
2022-02-02 19:13:31 +0000

    Include promela parser test from local branch

--- tests/test_promela_parser.py
+++ tests/test_promela_parser.py
@@ -1,37 +1,43 @@
 from ltlplanner import promela
 
-sample_promela = """never { /* []<>(a && b) && <> c */
+sample_promela = """
+never { /* []<>a && []<>b */
 T0_init:
     if
     :: (1) -> goto T0_init
-    :: (c) -> goto T0_S4
+    :: (a) -> goto T1_S1
+    :: (a && b) -> goto accept_S1
     fi;
-T0_S4:
+T1_S1:
     if
-    :: (1) -> goto T0_S4
-    :: (a && b) -> goto accept_S4
+    :: (1) -> goto T1_S1
+    :: (b) -> goto accept_S1
     fi;
-accept_S4:
+accept_S1:
     if
-    :: (1) -> goto T0_S4
-    :: (a && b) -> goto accept_S4
+    :: (1) -> goto T0_init
+    :: (a) -> goto T1_S1
+    :: (a && b) -> goto accept_S1
     fi;
-}"""
+}
+"""
 
 
-def test_basic_parsing():
-    actual = promela.parse(sample_promela)
+def test_promela_parser():
+    parsed = promela.parse(sample_promela)
     expected = promela.PromelaOutput(
         edges={
             ("T0_init", "T0_init"): "(1)",
-            ("T0_init", "T0_S4"): "(c)",
-            ("T0_S4", "T0_S4"): "(1)",
-            ("T0_S4", "accept_S4"): "(a && b)",
-            ("accept_S4", "T0_S4"): "(1)",
-            ("accept_S4", "accept_S4"): "(a && b)",
+            ("T0_init", "T1_S1"): "(a)",
+            ("T0_init", "accept_S1"): "(a && b)",
+            ("T1_S1", "T1_S1"): "(1)",
+            ("T1_S1", "accept_S1"): "(b)",
+            ("accept_S1", "T0_init"): "(1)",
+            ("accept_S1", "T1_S1"): "(a)",
+            ("accept_S1", "accept_S1"): "(a && b)",
         },
-        states={"T0_init", "T0_S4", "accept_S4"},
-        initial_states={"T0_init"},
-        accept_states={"accept_S4"}
+        states={"T0_init", "T1_S1", "accept_S1"},
+        initial_states = {'T0_init'},
+        accept_states={"accept_S1"}
     )
-    assert actual == expected
+    assert parsed == expected
