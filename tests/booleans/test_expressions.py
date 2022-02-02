bf6752dbb5febde29dda30d5ba26c5a4b8533d5f
Haukur <haukur.heidarsson@arionbanki.is>
2022-02-02 22:01:26 +0000

    Move boolean tests around

--- tests/booleans/test_expressions.py
+++ tests/booleans/test_expressions.py
@@ -11,3 +11,10 @@ def test_boolean_distance():
     expr = parser.parse(formula)
     assert expr.distance(["a"]) == 0, "Distance from a=True, b=False: 0"
     assert expr.distance(["b"]) == 1, "Distance from a=False, b=True: 1"
+
+
+def test_negative_normal_form():
+    formula = "!(a || b)"
+    actual = parser.parse(formula).nnf()
+    expected = ANDExpression(NotSymbolExpression("a"), NotSymbolExpression("b"))
+    assert actual == expected
--- tests/test_boolean_nnf.py
+++
@@ -1,9 +0,0 @@
-from ltlplanner.booleans import parser
-from ltlplanner.booleans.expressions import *
-
-
-def test_negative_normal_form():
-    formula = "!(a || b)"
-    actual = parser.parse(formula).nnf()
-    expected = ANDExpression(NotSymbolExpression("a"), NotSymbolExpression("b"))
-    assert actual == expected
