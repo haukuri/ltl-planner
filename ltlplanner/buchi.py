bce0d656a442e5fa9f9c9db3bc5dd53f93ec9243
Haukur Ingi Heiðarsson <haukur@heidarsson.is>
2022-02-13 23:16:48 +0000

    Move concrete graph functionality into it's own class.

--- ltlplanner/buchi.py
+++ ltlplanner/buchi.py
@@ -1,26 +1,22 @@
-from collections import defaultdict
-
 from .booleans.expressions import Expression
-from .promela import PromelaOutput, parse as parse_promela, find_states
+from .promela import PromelaOutput, parse as parse_promela
 from .booleans.parser import parse as parse_boolean_expression
 from .ltl2ba_wrapper import run_ltl2ba
-from .graph import Graph
-from .utils import BidirectionalEdgeMap
+from .graph import ConcreteGraph
 
 empty_set = frozenset()
 
 
-class Buchi(Graph):
+class Buchi(ConcreteGraph):
     def __init__(self, promela_output: PromelaOutput):
+        super().__init__()
         self.initial = set(promela_output.initial_states)
         self.accept = set(promela_output.accept_states)
         guards = {}
-        edges = BidirectionalEdgeMap()
         for (src, dst), guard_formula in promela_output.edges.items():
             guards[(src, dst)] = parse_boolean_expression(guard_formula)
-            edges.add(src, dst)
+            self.add_edge(src, dst)
         self.__guards = guards
-        self.__edges = edges
 
     def guard(self, src, dst) -> Expression:
         """
@@ -28,12 +24,6 @@ class Buchi(Graph):
         Throws KeyError if the edge does not exist.
         """
         return self.__guards[(src, dst)]
-
-    def post(self, src):
-        return self.__edges.post(src)
-
-    def pre(self, dst):
-        return self.__edges.pre(dst)
 
 
 def from_ltl(formula: str) -> Buchi:
--- ltlplanner/graph.py
+++ ltlplanner/graph.py
@@ -2,10 +2,7 @@ from __future__ import annotations
 
 from abc import ABC, abstractmethod
 
-from .booleans.expressions import TrueExpression
-
-true_expression = TrueExpression()
-
+from .utils import BidirectionalEdgeMap
 
 class Graph(ABC):
     @abstractmethod
@@ -15,3 +12,16 @@ class Graph(ABC):
     @abstractmethod
     def pre(self, dst) -> set[str]:
         pass
+
+class ConcreteGraph(Graph):
+    def __init__(self):
+        self.__edges = BidirectionalEdgeMap()
+
+    def add_edge(self, src, dst):
+        return self.__edges.add(src, dst)
+
+    def post(self, src):
+        return self.__edges.post(src)
+
+    def pre(self, dst):
+        return self.__edges.pre(dst)
