01393aef8afb4e7ff2818e81f3756f80c03bef59
Haukur <haukur.heidarsson@arionbanki.is>
2022-02-24 09:35:12 +0000

    Add get_item_or_initialize to utils

--- ltlplanner/utils.py
+++ ltlplanner/utils.py
@@ -1,5 +1,18 @@
 from collections import defaultdict
 
+def get_item_or_initialize(collection, key, default_factory):
+    """
+    Tries to get an item from `collection`.
+
+    If it is not present, initializes it with value obtained from `default_factory`
+    and returns the initialized value.
+    """
+    try:
+        item = collection[key]
+    except KeyError:
+        item = default_factory()
+        collection[key] = item
+    return item
 
 class BidirectionalEdgeMap:
     def __init__(self):
