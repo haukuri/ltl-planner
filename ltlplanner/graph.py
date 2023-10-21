f5ba210ce7b261dd8c20ccea6e3941da2d00683b
Haukur Ingi Heiðarsson <haukur@heidarsson.is>
2023-10-21 23:48:56 +0000

    Add type annotations in graph.py

--- ltlplanner/graph.py
+++ ltlplanner/graph.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from typing import Iterator
 
 
 def _defaultdict_of_dict():
@@ -6,31 +7,40 @@ def _defaultdict_of_dict():
 
 
 class BidirectionalEdgeMap:
+    __post: defaultdict[str, set[str]]
+    __pre: defaultdict[str, set[str]]
+
     def __init__(self):
         self.__post = defaultdict(set)
         self.__pre = defaultdict(set)
 
-    def add(self, a, b):
+    def add(self, a: str, b: str):
         self.post(a).add(b)
         self.pre(b).add(a)
 
-    def remove(self, a, b):
+    def remove(self, a: str, b: str):
         self.post(a).remove(b)
         self.pre(b).remove(a)
 
-    def post(self, a):
+    def post(self, a: str) -> set[str]:
         return self.__post[a]
 
-    def pre(self, b):
+    def pre(self, b: str) -> set[str]:
         return self.__pre[b]
 
-    def edges(self):
+    def edges(self) -> Iterator[tuple[str, str]]:
         for a, post in self.__post.items():
             for b in post:
                 yield (a, b)
 
 
 class Graph:
+    edge_attrs: defaultdict[tuple[str, str], dict]
+    node_attrs: defaultdict[str, dict]
+    initial: set[str]
+    accept: set[str]
+    __edges: BidirectionalEdgeMap
+
     def __init__(self):
         self.__edges = BidirectionalEdgeMap()
         self.edge_attrs = defaultdict(_defaultdict_of_dict)
@@ -43,16 +53,16 @@ class Graph:
         if attributes:
             self.edge_attrs[(src, dst)].update(attributes)
 
-    def post(self, src):
+    def post(self, src) -> set[str]:
         return self.__edges.post(src)
 
-    def pre(self, dst):
+    def pre(self, dst) -> set[str]:
         return self.__edges.pre(dst)
 
-    def edges(self):
+    def edges(self) -> Iterator[tuple[str, str]]:
         return self.__edges.edges()
 
-    def nodes(self):
+    def nodes(self) -> set[str]:
         nodes = set()
         for (s, d) in self.edges():
             nodes.add(s)
